36e7ae661be67eb57d6593af961af168b5947ed1
nbradf2 <nbradf2@gmail.com>
2017-09-28 18:00:15 -0500

    update API functionality to display house and senate members

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-//'use strict'
-
-// VARIABLES
-
-let validateState = ['alabama', 'al', 'alaska', 'ak', 'arizona', 'az', 'arkansas', 'ar', 'california', 'ca', 'colorado', 'co', 'connecticut', 'ct', 'delaware', 'de', 'florida', 'fl', 'georgia', 'ga', 'hawaii', 'hi', 'idaho', 'id', 'illinois', 'il', 'indiana', 'in', 'iowa', 'ia', 'kansas', 'ks', 'kentucky', 'ky', 'louisiana', 'la', 'maine', 'me', 'maryland', 'md', 'massachusetts', 'ma', 'michigan', 'mi', 'minnesota', 'mn', 'mississippi', 'ms', 'missouri', 'mo', 'montana', 'mt', 'nebraska', 'ne', 'nevada', 'nv', 'new hampshire', 'nv', 'new jersey', 'nj', 'new mexico', 'nm', 'new york', 'ny', 'north carolina', 'nc', 'north dakota', 'nd', 'ohio', 'oh', 'oklahoma', 'ok', 'oregon', 'or', 'pennsylvania', 'pa', 'rhode island', 'ri', 'south carolina', 'sc', 'south dakota', 'sd', 'tennessee', 'tn', 'texas', 'tx', 'utah', 'ut', 'vermont', 'vt', 'virginia', 'va', 'washington', 'wa', 'west virginia', 'wv', 'wisconsin', 'wi', 'wyoming', 'wy'];
-
-const errorMessage = $(`<p class="invalid-text">That's not a state!  Enter the full name (i.e., "Illinois"), or the abbreviation (i.e., "IL)</p>`);
-
-const blankMessage = $(`<p class="invalid-text">Uh oh! Looks like you forgot to add the state you're looking for!</p>`)
+/******************************
+ VARIABLES
+ *****************************/
 
 let userState = '';
 
@@ -27,13 +21,16 @@ function getStateInput() {
 
 };
 
-// get ProPublica API:
+// get ProPublica API (House):
 
-function getProPublicaInfo(userState) {
-    var result = $.ajax({
+function getProPublicaHouse(userState) {
+
+    // House
+
+    var resultHouse = $.ajax({
             /* https://projects.propublica.org/api-docs/congress-api/members/#get-current-members-by-statedistrict*/
             //url: "https://api.propublica.org/congress/v1/members/senate/IL/current.json",
-            url: "https://api.propublica.org/congress/v1/members/house/" + userState + "/current.json",
+            url: `https://api.propublica.org/congress/v1/members/house/${userState}/current.json`,
             type: "GET",
             dataType: 'json',
             headers: {
@@ -41,10 +38,11 @@ function getProPublicaInfo(userState) {
             }
         })
         /* if the call is successful (status 200 OK) show results */
-        .done(function (result) {
+        .done(function (resultHouse) {
             /* if the results are meeningful, we can just console.log them */
-            console.log(result);
-            displayHouseResults(result);
+            console.log(resultHouse);
+
+            displayHouseResults(resultHouse);
         })
         /* if the call is NOT successful show errors */
         .fail(function (jqXHR, error, errorThrown) {
@@ -55,15 +53,59 @@ function getProPublicaInfo(userState) {
 };
 
 function displayHouseResults(houseArray) {
+
     let buildHouseMembers = "";
 
     $.each(houseArray.results, function (houseArrayKey, houseArrayValue) {
         buildHouseMembers +=
             `<li><a href="#">${houseArrayValue.name}</a></li>`
-    })
 
-    //show in HTML
-    $('#house ul').html(buildHouseMembers)
+        //show in HTML
+        $('#house ul').html(buildHouseMembers)
+    })
+}
+
+
+// Senate
+function getProPublicaSenate(userState) {
+
+    var resultSenate = $.ajax({
+            /* https://projects.propublica.org/api-docs/congress-api/members/#get-current-members-by-statedistrict*/
+            //url: "https://api.propublica.org/congress/v1/members/senate/IL/current.json",
+            url: `https://api.propublica.org/congress/v1/members/senate/${userState}/current.json`,
+            type: "GET",
+            dataType: 'json',
+            headers: {
+                'X-API-Key': 'YNoO8cdS5NIhOruok98nV3s6bBzGoJCynVa97XnW'
+            }
+        })
+        /* if the call is successful (status 200 OK) show results */
+        .done(function (resultSenate) {
+            /* if the results are meeningful, we can just console.log them */
+            console.log(resultSenate);
+
+            displaySenateResults(resultSenate);
+        })
+        /* if the call is NOT successful show errors */
+        .fail(function (jqXHR, error, errorThrown) {
+            console.log(jqXHR);
+            console.log(error);
+            console.log(errorThrown);
+        });
+};
+
+
+function displaySenateResults(senateArray) {
+
+    let buildSenateMembers = "";
+
+    $.each(senateArray.results, function (senateArrayKey, senateArrayValue) {
+        buildSenateMembers +=
+            `<li><a href="#">${senateArrayValue.name}</a></li>`
+
+        //show in HTML
+        $('#senate ul').html(buildSenateMembers)
+    })
 }
 
 
@@ -85,9 +127,17 @@ $(document).ready(function () {
     $('#my-state').on('change', function () {
         event.preventDefault();
 
+        // Get user input (state)
         getStateInput();
 
+        // Call ProPublica API for House and Senate name info
+        getProPublicaHouse(userState);
+        getProPublicaSenate(userState);
 
+        // show #list-names section
+        $("#results-section").hide();
+        $("#state-form").hide();
+        $("#list-names").show();
 
     })
 
