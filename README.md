21a4ce9780f3c9de1cbadac32539cca585590bda
nbradf2 <nbradf2@gmail.com>
2017-10-18 18:46:58 -0500

    clean up js and html files, update README

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
+# My Politician
+<a href="https://www.thinkful.com">Thinkful</a> API Capstone Project - Front End app integrating ProPublica Congress API
+
+![landing page](screenshots/my-pol-landing-page.png)
+
+![list of state representatives page](screenshots/my-pol-rep-list.png)
+
+![results page](screenshots/my-pol-results.png)
+
 ## Background
 Having little to no knowledge of who my state politicians are, I wanted to create an app that will look up politicians by state, and provide information about them.
 
 ## Use Case
 This app will help users find out who their state politicians are, what political party they represent, general information about them, and recent news articles relating to them.
+
+## Functionality
+This app's functionality includes:
+    * Search for House Representatives by State
+    * Search for Senate Representatives by State
+    * Return information from Wikipedia and news articles from the New York Times
+
+## Technology
+### Front End
+    * HTML
+    * CSS
+    * JavaScript
+    * jQuery
+
+The app uses AJAX JSON calls to <a href="https://projects.propublica.org/api-docs/congress-api/">ProPublica Congress</a>, <a href="https://www.mediawiki.org/wiki/API:Main_page">Wikipedia</a>, and the <a href="http://developer.nytimes.com/">New York Times</a> to return search results.
+
+## Responsive
+This app is built to be responsive across mobile, tablet, laptop, and desktop screens.
+
+## Development
+Future updates are expected to include:
+    * Search by zipcode
+    * Include local government
--- app.js
+++ app.js
@@ -11,86 +11,49 @@ let politicianNameWithMiddle = '';
  FUNCTION DEFINITIONS
  *****************************/
 
-// To get user input (state):
-
 function getStateInput() {
-
-    // get value of selected state:
     userState = $('#my-state').val();
-
-    // test
-    console.log(userState);
-
 };
 
 // Set url to id of selected name with politicianId variable
-
 function getAllUrlParams(url) {
-
-    // get query string from url (optional) or window
     var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
-
-    // we'll store the parameters here
     var obj = {};
 
-    // if query string exists
     if (queryString) {
-
-        // stuff after # is not part of query string, so get rid of it
         queryString = queryString.split('#')[0];
-
-        // split our query string into its component parts
         var arr = queryString.split('&');
 
         for (var i = 0; i < arr.length; i++) {
-            // separate the keys and the values
             var a = arr[i].split('=');
-
-            // in case params look like: list[]=thing1&list[]=thing2
             var paramNum = undefined;
             var paramName = a[0].replace(/\[\d*\]/, function (v) {
                 paramNum = v.slice(1, -1);
                 return '';
             });
 
-            // set parameter value (use 'true' if empty)
             var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
 
-            // if parameter name already exists
             if (obj[paramName]) {
-                // convert value to array (if still string)
                 if (typeof obj[paramName] === 'string') {
                     obj[paramName] = [obj[paramName]];
                 }
-                // if no array index number specified...
                 if (typeof paramNum === 'undefined') {
-                    // put the value on the end of the array
                     obj[paramName].push(paramValue);
-                }
-                // if array index number specified...
-                else {
-                    // put the value at that index number
+                } else {
                     obj[paramName][paramNum] = paramValue;
                 }
-            }
-            // if param name doesn't exist yet, set it
-            else {
+            } else {
                 obj[paramName] = paramValue;
             }
         }
     }
-
     return obj;
 }
 
-// get ProPublica API (House):
-
 function getProPublicaHouse(userState) {
 
-    // House
-
     var resultHouse = $.ajax({
-
             url: `https://api.propublica.org/congress/v1/members/house/${userState}/current.json`,
             type: "GET",
             dataType: 'json',
@@ -98,15 +61,9 @@ function getProPublicaHouse(userState) {
                 'X-API-Key': 'YNoO8cdS5NIhOruok98nV3s6bBzGoJCynVa97XnW'
             }
         })
-        /* if the call is successful (status 200 OK) show results */
         .done(function (resultHouse) {
-            /* if the results are meaningful, we can just console.log them */
-
-            console.log(resultHouse);
-
             displayHouseResults(resultHouse);
         })
-        /* if the call is NOT successful show errors */
         .fail(function (jqXHR, error, errorThrown) {
             console.log(jqXHR);
             console.log(error);
@@ -115,23 +72,19 @@ function getProPublicaHouse(userState) {
 };
 
 function displayHouseResults(houseArray) {
-
     let buildHouseMembers = "";
 
     $.each(houseArray.results, function (houseArrayKey, houseArrayValue) {
         buildHouseMembers +=
             `<li><a href="./?id=${encodeURI(houseArrayValue.id)}">${houseArrayValue.name} (${houseArrayValue.party})</a></li>`
 
-        //show in HTML
         $('#house ul').html(buildHouseMembers)
     })
 }
 
-// Senate
+
 function getProPublicaSenate(userState) {
-
     var resultSenate = $.ajax({
-
             url: `https://api.propublica.org/congress/v1/members/senate/${userState}/current.json`,
             type: "GET",
             dataType: 'json',
@@ -139,14 +92,9 @@ function getProPublicaSenate(userState) {
                 'X-API-Key': 'YNoO8cdS5NIhOruok98nV3s6bBzGoJCynVa97XnW'
             }
         })
-        /* if the call is successful (status 200 OK) show results */
         .done(function (resultSenate) {
-            /* if the results are meaningful, we can just console.log them */
-            console.log(resultSenate);
-
             displaySenateResults(resultSenate);
         })
-        /* if the call is NOT successful show errors */
         .fail(function (jqXHR, error, errorThrown) {
             console.log(jqXHR);
             console.log(error);
@@ -155,22 +103,17 @@ function getProPublicaSenate(userState) {
 };
 
 function displaySenateResults(senateArray) {
-
     let buildSenateMembers = "";
 
     $.each(senateArray.results, function (senateArrayKey, senateArrayValue) {
         buildSenateMembers +=
             `<li><a href="./?id=${encodeURI(senateArrayValue.id)}">${senateArrayValue.name} (${senateArrayValue.party})</a></li>`
 
-        //show in HTML
         $('#senate ul').html(buildSenateMembers)
     })
 }
 
-// Call ProPublica API for individual members using politicianId variable
-
 function getIndividualPolitician(politicianId) {
-
     var resultIndividualPolitician = $.ajax({
 
             url: `https://api.propublica.org/congress/v1/members/${politicianId}.json`,
@@ -180,14 +123,9 @@ function getIndividualPolitician(politicianId) {
                 'X-API-Key': 'YNoO8cdS5NIhOruok98nV3s6bBzGoJCynVa97XnW'
             }
         })
-        /* if the call is successful (status 200 OK) show results */
         .done(function (resultIndividualPolitician) {
-            /* if the results are meaningful, we can just console.log them */
-            console.log(resultIndividualPolitician);
-
             displayIndividualResults(resultIndividualPolitician);
         })
-        /* if the call is NOT successful show errors */
         .fail(function (jqXHR, error, errorThrown) {
             console.log(jqXHR);
             console.log(error);
@@ -195,22 +133,13 @@ function getIndividualPolitician(politicianId) {
         });
 };
 
-// Display results of individual politician call
 function displayIndividualResults(individualArray) {
-
     $.each(individualArray.results, function (individualArrayKey, individualArrayValue) {
 
-        // store name in variable
         politicianName = `${individualArrayValue.first_name} ${individualArrayValue.last_name}`;
-
         politicianNameWithMiddle = `${individualArrayValue.first_name} ${individualArrayValue.middle_name} ${individualArrayValue.last_name}`;
 
-        console.log(politicianName);
-
-        // Politician name - heading
         $('#pol-name').append(`<a href="${individualArrayValue.url}" target="_blank">${politicianName}</a>`);
-
-        // Image
         $('#pol-image').append(`<img src="https://theunitedstates.io/images/congress/225x275/${politicianId}.jpg" alt="Image of ${individualArrayValue.first_name} ${individualArrayValue.last_name}">`)
 
         if (individualArrayValue.current_party === "D") {
@@ -223,8 +152,6 @@ function displayIndividualResults(individualArray) {
             $('#party').append('Independent');
         }
 
-        // Contact Info
-
         $('#contact-info').append(
             `
 <p><i class = "fa fa-phone"></i>${individualArrayValue.roles[0].phone}</p>
@@ -235,41 +162,26 @@ function displayIndividualResults(individualArray) {
         );
     });
 
-    // Call Wiki API
     function getWikiApi(politicianName) {
-        //        var txt = politicianName;
         var resultWiki = $.ajax({
                 url: `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages|extracts&generator=search&exintro&explaintext&exsentences=1&exlimit=max&gsrlimit=10&callback=?&gsrsearch=${politicianName}`,
                 type: "GET",
-                //                    data: data,
                 dataType: 'jsonp',
             })
-
-            /* if the call is successful (status 200 OK) show results */
             .done(function (resultWiki) {
-                /* if the results are meaningful, we can just console.log them */
-                console.log(resultWiki);
-
-                // show in html
                 displayWikiArticle(resultWiki);
             })
-            /* if the call is NOT successful show errors */
             .fail(function (jqXHR, error, errorThrown) {
                 console.log(jqXHR);
                 console.log(error);
-
                 console.log(errorThrown);
             });
     };
 
-    // Display Wiki API
-
     function displayWikiArticle(wikiArray) {
-
         let buildWikiOutput = '';
 
         $.each(wikiArray.query.pages, function (wikiArrayKey, wikiArrayValue) {
-
             if (
                 wikiArrayValue.title == politicianName ||
                 wikiArrayValue.title == politicianNameWithMiddle ||
@@ -290,30 +202,19 @@ function displayIndividualResults(individualArray) {
         });
     }
 
-    // Call NY Times API
     function getTimesArticles(politicianName) {
-
         var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
         url += '?' + $.param({
             'api-key': '048e67fe7fe94ffb92aa6a58646dc462',
             'q': politicianName,
-
         });
-
         var timesResult = $.ajax({
-
                 url: url,
                 method: 'GET',
             })
-            /* if the call is successful (status 200 OK) show results */
             .done(function (timesResult) {
-                /* if the results are meaningful, we can just console.log them */
-                console.log(timesResult);
-
                 displayTimesArticle(timesResult);
-
             })
-            /* if the call is NOT successful show errors */
             .fail(function (jqXHR, error, errorThrown) {
                 console.log(jqXHR);
                 console.log(error);
@@ -321,14 +222,9 @@ function displayIndividualResults(individualArray) {
             });
     }
 
-    // display times articles
-
     function displayTimesArticle(timesArray) {
-
         let firstThree = timesArray.response.docs.slice(0, 3);
-
         $.each(firstThree, function (timesArrayKey, timesArrayValue) {
-
             let buildNyTimesOutput = '';
 
             buildNyTimesOutput += `<article class="col-4 ny-times-style">`;
@@ -337,10 +233,7 @@ function displayIndividualResults(individualArray) {
             buildNyTimesOutput += `<p>${timesArrayValue.snippet}</p>`;
             buildNyTimesOutput += `</article>`;
 
-            //show in html
-
             $('#news-section').append(buildNyTimesOutput);
-
         });
     }
     getWikiApi(politicianName);
@@ -353,26 +246,18 @@ FUNCTION USAGE
 $(document).ready(function () {
 
     // #STATE-FORM
-
-    // At start, only show form (id: #state-form)
-
     politicianId = decodeURI(getAllUrlParams().id);
 
     console.log("Bioguide ID ==> ", politicianId);
     console.log("urlParams ==> ", Object.keys(getAllUrlParams()).length);
 
-    // Conditional to set #list-names if name is clicked
-
-    // no id defined
     if ((Object.keys(getAllUrlParams()).length == 0) || (politicianId == '') || (politicianId === undefined)) {
         console.log("id-undefined");
         $("#list-names").hide();
         $("#results-section").hide();
         $(".return-link-div").hide();
         $("#state-form").show();
-    }
-    // id defined
-    else {
+    } else {
         console.log("id-defined");
         $("#state-form").hide();
         $("#list-names").hide();
@@ -380,53 +265,27 @@ $(document).ready(function () {
         $(".return-link-div").show();
         $("#results-section").show();
 
-        // CALL RESULTS FUNCTIONS HERE
-        // Call ProPublica API for individual call
         getIndividualPolitician(politicianId);
     }
-
-    // On click #state-submit activate getStateInput() function and show #list-names section
 
     $('#my-state').on('change', function () {
         event.preventDefault();
 
-        // Get user input (state)
         getStateInput();
-
-        // Call ProPublica API for House and Senate name info
         getProPublicaHouse(userState);
         getProPublicaSenate(userState);
 
-        // show# list - names section
         $("#results-section").hide();
         $("#state-form").hide();
         $("#previous-page").hide();
         $(".return-link-div").show();
         $("#list-names").show();
-
     });
 
-    // Set Start-Over button
-
     $("#start-over").click(function () {
-
         $("#list-names").hide();
         $("#results-section").hide();
         $(".return-link-div").hide();
         $("#state-form").show();
-
     })
-
-    //    // Set Previous-Page Button
-    //    $("#previous-page").click(function () {
-    //
-    //        // show #list-names section
-    //        $("#results-section").hide();
-    //        $("#state-form").hide();
-    //        $("#list-names").show();
-    //        $("#previous-page").hide();
-    //        $(".return-link-div").show();
-    //
-    //    })
-
 })
